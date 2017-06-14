e5274fd178301898579c8b14dbdc1fdad3af1b89
Filip <schwafil@fel.cvut.cz>
2017-06-14 22:40:14 +0200

    small changes

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 let canvas = document.getElementById("brickCanvas");
 let ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
-canvas.height = window.innerHeight*0.7;
+canvas.height = window.innerHeight * 0.7;
 
 let dx = 2;
 let dy = -2;
@@ -13,11 +13,12 @@ let rightPressed = false;
 let leftPressed = false;
 const brickRowCount = 10;
 const brickColumnCount = 10;
-let ballRadius = canvas.width/100,brickOffsetTop = canvas.height*0.07,brickOffsetLeft = canvas.width*0.03,
+let ballRadius = canvas.width / 100, brickOffsetTop = canvas.height * 0.07, brickOffsetLeft = canvas.width * 0.03,
     brickWidth = (canvas.width - 2 * brickOffsetLeft) / (brickRowCount + 1),
-    brickHeight = (canvas.height -  4*brickOffsetTop)/(brickColumnCount+1),
+    brickHeight = (canvas.height - 4 * brickOffsetTop) / (brickColumnCount + 1),
     brickPadding = brickWidth / brickRowCount;
 let ballCount = 10;
+let magic = Math.ceil(ballRadius);
 let score = 0, highscore = 0;
 
 let mouseX = 0;
@@ -29,12 +30,12 @@ let play = false;
 const endGame = new Audio("gameover.wav")
 const bounce = new Audio("bounce3.wav");
 const beeps = [];
-for (let bee = 0;bee<ballCount;bee++){
+for (let bee = 0; bee < ballCount; bee++) {
     beeps[bee] = bounce.cloneNode();
 }
 const bounce2 = new Audio("bounce2.wav");
 const beeps2 = [];
-for (let bee = 0;bee<ballCount;bee++){
+for (let bee = 0; bee < ballCount; bee++) {
     beeps2[bee] = bounce2.cloneNode();
 }
 class Brick {
@@ -52,8 +53,8 @@ function initBricks() {
             bricks[c][r] = new Brick(0, 0, 0);//{ x: 0, y: 0, status: 1 };
         }
     }
-    for  (let r = 0; r < brickRowCount; r++) {
-        bricks[0][r].hits = (Math.random()*10) >=1 ? ballCount : 0;
+    for (let r = 0; r < brickRowCount; r++) {
+        bricks[0][r].hits = (Math.random() * 10) >= 1 ? ballCount : 0;
     }
 }
 initBricks();
@@ -61,24 +62,24 @@ initBricks();
 
 function shiftAddBricks() {
 
-    for  (let r = 0; r < brickRowCount; r++) {
-        if(bricks[ballCount-1][r].hits>0) return true;//if there is a brick with hits on the last row, end the game
+    for (let r = 0; r < brickRowCount; r++) {
+        if (bricks[ballCount - 1][r].hits > 0) return true;//if there is a brick with hits on the last row, end the game
     }
-    for (let c = brickColumnCount-1; c > 0; c--) {
+    for (let c = brickColumnCount - 1; c > 0; c--) {
         for (let r = 0; r < brickRowCount; r++) {
             let brick = bricks[c][r];
-            let brickShift = bricks[c-1][r];
+            let brickShift = bricks[c - 1][r];
             brick.x = brickShift.x;
             brick.y = brickShift.y;
             brick.hits = brickShift.hits;
         }
     }
-    for  (let r = 0; r < brickRowCount; r++) {
-        bricks[0][r].hits = (Math.random()*10) >=1 ? ballCount : 0;
+    for (let r = 0; r < brickRowCount; r++) {
+        bricks[0][r].hits = (Math.random() * 10) >= 1 ? ballCount : 0;
     }
 }
 class Ball {
-    constructor(x, y, dx, dy,inGame,putInto) {
+    constructor(x, y, dx, dy, inGame, putInto) {
         this.x = x;
         this.y = y;
         this.dx = dx;
@@ -97,24 +98,25 @@ document.addEventListener("keyup", keyUpHandler, false);
 canvas.addEventListener("mousemove", mouseMoveHandler, false);
 canvas.addEventListener("click", mouseClickHandler, false);
 function resizeCanvas() {
-    aimX = (aimX/canvas.width)*window.innerWidth;
+    aimX = (aimX / canvas.width) * window.innerWidth;
     canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight*0.7;
-    ballRadius = canvas.width/100,brickOffsetTop = canvas.height*0.07,brickOffsetLeft = canvas.width*0.03,
-        brickWidth = (canvas.width - 2 * brickOffsetLeft) / (brickRowCount + 1), brickHeight = (canvas.height -  4*brickOffsetTop)/(brickColumnCount+1),
+    canvas.height = window.innerHeight * 0.7;
+    ballRadius = canvas.width / 100, brickOffsetTop = canvas.height * 0.07, brickOffsetLeft = canvas.width * 0.03,
+        brickWidth = (canvas.width - 2 * brickOffsetLeft) / (brickRowCount + 1), brickHeight = (canvas.height - 4 * brickOffsetTop) / (brickColumnCount + 1),
         brickPadding = brickWidth / brickRowCount;
     aimY = canvas.height - brickOffsetTop;
+    magic = Math.ceil(ballRadius);
 }
 function CASStorage() {
-    if(typeof(Storage) !== "undefined") {
-        if (localStorage.getItem("highscore")!==null) {
+    if (typeof(Storage) !== "undefined") {
+        if (localStorage.getItem("highscore") !== null) {
             highscore = localStorage.getItem("highscore");
-            if(score>highscore){
+            if (score > highscore) {
                 highscore = score;
-                localStorage.setItem("highscore",highscore);
+                localStorage.setItem("highscore", highscore);
             }
         } else {
-            localStorage.setItem("highscore",highscore);
+            localStorage.setItem("highscore", highscore);
         }
 
     }
@@ -166,17 +168,17 @@ function collisionDetection() {//
     for (let col = 0; col < brickColumnCount; col++) {
         for (let row = 0; row < brickRowCount; row++) {
             let brick = bricks[col][row];
-            if (brick.hits >0) {
+            if (brick.hits > 0) {
                 for (let ba = 0; ba < ballCount; ba++) {
                     let ball = balls[ba];
                     if (!ball.inGame) continue;
 
                     //new mechanics to detect both x y bounce
                     ///bouncing in x axis
-                    if( (ball.x + ball.dx + ballRadius >= brick.x) && (ball.x + ballRadius <= brick.x) ||
+                    if ((ball.x + ball.dx + ballRadius >= brick.x) && (ball.x + ballRadius <= brick.x) ||
                         (ball.x + ball.dx - ballRadius <= brick.x + brickWidth) && (ball.x - ballRadius >= brick.x + brickWidth)
-                    ){
-                        if ((ball.y + ball.dy - ballRadius <=brick.y + brickHeight) && (ball.y + ball.dy + ballRadius >=brick.y)){
+                    ) {
+                        if ((ball.y + ball.dy - ballRadius <= brick.y + brickHeight) && (ball.y + ball.dy + ballRadius >= brick.y)) {
                             ball.dx = -ball.dx;
                             brick.hits--;
                             score++;
@@ -185,10 +187,10 @@ function collisionDetection() {//
                             continue;
                         }
                     }
-                    if( (ball.y + ball.dy + ballRadius >= brick.y) && (ball.y + ballRadius <= brick.y) ||
+                    if ((ball.y + ball.dy + ballRadius >= brick.y) && (ball.y + ballRadius <= brick.y) ||
                         (ball.y + ball.dy - ballRadius <= brick.y + brickHeight) && (ball.y - ballRadius >= brick.y + brickHeight)
-                    ){
-                        if ((ball.x + ball.dx - ballRadius <=brick.x + brickWidth) && (ball.x + ball.dx + ballRadius >=brick.x)){
+                    ) {
+                        if ((ball.x + ball.dx - ballRadius <= brick.x + brickWidth) && (ball.x + ball.dx + ballRadius >= brick.x)) {
                             ball.dy = -ball.dy;
                             brick.hits--;
                             beeps[ba].play();//bounce.play();
@@ -196,21 +198,6 @@ function collisionDetection() {//
                             CASStorage();
                         }
                     }
-
-
-
-
-
-                  /*  if (ball.x > brick.x && ball.x < brick.x + brickWidth && ball.y > brick.y && ball.y < brick.y + brickHeight) {
-                        ball.dy = -ball.dy;
-                        brick.hits--;
-                        score++;
-                        if (score === brickRowCount * brickColumnCount *10) {
-                            alert("YOU WIN, CONGRATS!");
-                            document.location.reload();
-                        }
-                    }
-                    */
                 }
             }
         }
@@ -220,7 +207,7 @@ function collisionDetection() {//
 
 
 function drawBall(xx, yy) {
-    if(xx<0 || xx >canvas.width || yy <0 || yy > canvas.height) return;
+    if (xx < 0 || xx > canvas.width || yy < 0 || yy > canvas.height) return;
     ctx.beginPath();
     ctx.arc(xx, yy, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = "#ffffff";
@@ -237,7 +224,7 @@ function drawBottom() {
 function drawBricks() {
     for (let c = 0; c < brickColumnCount; c++) {
         for (let r = 0; r < brickRowCount; r++) {
-            if (bricks[c][r].hits >0) {
+            if (bricks[c][r].hits > 0) {
                 const brickX = (r * (brickWidth + brickPadding)) + brickOffsetLeft;
                 const brickY = (c * (brickHeight + brickPadding)) + brickOffsetTop;
                 const brickTextX = brickX + brickWidth / 2;
@@ -251,7 +238,7 @@ function drawBricks() {
                 ctx.closePath();
                 ctx.font = "14px Arial";
                 ctx.fillStyle = "#ffffff";
-                ctx.textAlign="center";
+                ctx.textAlign = "center";
                 ctx.fillText(bricks[c][r].hits, brickTextX, brickTextY);
             }
         }
@@ -259,10 +246,10 @@ function drawBricks() {
 }
 function drawScore() {
     /*ctx.font = "16px Arial";
-    ctx.fillStyle = "#dd8b00";
-    ctx.textAlign="left";
-    ctx.fillText("Score: " + score + " HighScore: "+highscore, 8, 20);*/
-    document.querySelector("#info").innerText = "Score: " + score + " HighScore: "+highscore;
+     ctx.fillStyle = "#dd8b00";
+     ctx.textAlign="left";
+     ctx.fillText("Score: " + score + " HighScore: "+highscore, 8, 20);*/
+    document.querySelector("#info").innerText = "Score: " + score + " HighScore: " + highscore;
 }
 
 function drawMouseAim() {
@@ -302,41 +289,45 @@ function draw() {
             else if (ball.y + ball.dy > canvas.height - ballRadius) { // ball gets out of bottom border
 //if first ball, --live, set x etc, else nothing?
 
-                if(!ball.putInto) {
+                if (!ball.putInto) {
                     ball.inGame = false;
                 }
 
             } else {
                 balling = true; //if there is a ball still pinging
-               if(!ball.putInto){
-                   ball.x += ball.dx;
+                if (!ball.putInto) {
+                    ball.x += ball.dx;
                     ball.y += ball.dy;
-               }else{
+                } else {
 
-                   if(releaseCounter%10 === 0 ){
-                       console.log(releaseCounter/10);
-                       if( b===releaseCounter/10){
-                       //console.log("putting ball "+b);
-                       ball.putInto = false;
-                       ball.inGame = true;
-                   }}
-               }
+                    if (releaseCounter % magic === 0) {
+                        //console.log(releaseCounter/10);
+                        if (b === releaseCounter / magic) {
+                            console.log(ballRadius);
+                            //console.log("putting ball "+b);
+                            ball.putInto = false;
+                            ball.inGame = true;
+                        }
+                    }
+                }
             }
 
         }
         if (!balling) {
             if (shiftAddBricks()) {
-               // alert("GAME OVER");
-                document.querySelector("#info").innerText = "GAME OVER!! Score: " + score + " HighScore: "+highscore;
+                // alert("GAME OVER");
+                document.querySelector("#info").innerText = "GAME OVER!! Score: " + score + " HighScore: " + highscore;
                 endGame.play();
 
 
-                setTimeout(function() { document.location.reload(); }, 500);
+                setTimeout(function () {
+                    document.location.reload();
+                }, 500);
             }
             play = false;
             aimX = balls[0].x;
 
-        }else releaseCounter++;
+        } else releaseCounter++;
 
     }
 
@@ -351,7 +342,7 @@ const menuButton = document.querySelector('.menu-button');
 
 menuButton.addEventListener('click', toggleMenuState);
 
-function toggleMenuState () {
+function toggleMenuState() {
     menuVisible = !menuVisible;
     if (menuVisible) {
         document.body.classList.add('menu-visible');
